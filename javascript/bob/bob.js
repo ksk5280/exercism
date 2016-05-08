ef8de1f74b791c3b1738da0edc0d583cc7a05c1c
Kimiko Kano <kskano@gmail.com>
2016-05-07 23:46:33 -0600

    Bob refactor

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,11 +1,11 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-  if (!/\S/.test(input)) {
+  if (input.trim().length === 0) {
     return 'Fine. Be that way!';
   } else if (/[a-zA-Z]/.test(input) && input.toUpperCase() === input) {
     return 'Whoa, chill out!';
-  } else if (input.substr(-1) === '?') {
+  } else if (input.endsWith('?')) {
     return 'Sure.';
   } else {
     return 'Whatever.';
